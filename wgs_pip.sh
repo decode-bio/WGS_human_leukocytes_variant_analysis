9cd412a7f16a430d7e7f2a60462382ad4140cce2
Sneha Deshmukh <sneha.biocode@gmail.com>
2025-07-30 10:53:47 +0530

    Add files via upload

--- wgs_pip.sh
+++ wgs_pip.sh
@@ -4,31 +4,31 @@
 
 # Step 1: Fetching the data from the database with the help of fasterq (it reduces one step)
 
-fasterq-dump SRR34149094
+#fasterq-dump SRR34149094
 
 # Step 2: After downloading the file we will first look for the quality control of sequence.
 
-fastqc *.fastq
+#fastqc *.fastq
 
 # Step 3: The sequence need not to be trimeed as the quality seems fine. But lets compare before and after trimming and then decide.
 
-Trimmomatic=/home/sne_desh/Tools/Trimmomatic-0.39/trimmomatic-0.39.jar
-read1=/home/sne_desh/WGS/P1/SRR34149094_1.fastq
-read2=/home/sne_desh/WGS/P1/SRR34149094_2.fastq
-OP_read1_P=/home/sne_desh/WGS/P1/SRR34149094_1_P.fastq
-OP_read1_U=/home/sne_desh/WGS/P1/SRR34149094_1_U.fastq
-OP_read2_P=/home/sne_desh/WGS/P1/SRR34149094_1_P.fastq
-OP_read2_U=/home/sne_desh/WGS/P1/SRR34149094_1_U.fastq
+#Trimmomatic=/home/sne_desh/Tools/Trimmomatic-0.39/trimmomatic-0.39.jar
+#read1=/home/sne_desh/WGS/P1/SRR34149094_1.fastq
+#read2=/home/sne_desh/WGS/P1/SRR34149094_2.fastq
+#OP_read1_P=/home/sne_desh/WGS/P1/SRR34149094_1_P.fastq
+#OP_read1_U=/home/sne_desh/WGS/P1/SRR34149094_1_U.fastq
+#OP_read2_P=/home/sne_desh/WGS/P1/SRR34149094_1_P.fastq
+#OP_read2_U=/home/sne_desh/WGS/P1/SRR34149094_1_U.fastq
 
-java -jar $Trimmomatic PE $read1 $read2 $OP_read1_P $OP_read1_U $OP_read2_P $OP_read2_U \
-ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+#java -jar $Trimmomatic PE $read1 $read2 $OP_read1_P $OP_read1_U $OP_read2_P $OP_read2_U \
+#ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
 # Note: After comparing the both trimmed and untrimmed sequences quality it seems to me that the untrimmed sequence is fine to proceed further.
 
 # Step4: Hisat2 alignement
 
-/usr/bin/hisat2 -q -x /home/sne_desh/HISAT2/grch38/genome -1 /home/sne_desh/WGS/P1/SRR34149094_1.fastq -2 /home/sne_desh/WGS/P1/SRR34149094_2.fastq \
- -S SRR341449094.sam
+#/usr/bin/hisat2 -q -x /home/sne_desh/HISAT2/grch38/genome -1 /home/sne_desh/WGS/P1/SRR34149094_1.fastq -2 /home/sne_desh/WGS/P1/SRR34149094_2.fastq \
+ #-S SRR34149094.sam
 
 # The output showing as terminated, because of the RAM, I have been using 8 GB but it required more. Lets just wait\
 # till I get new RAM get installed.
@@ -38,17 +38,24 @@ ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLE
 # Step 5: Samtool and duplicate removal
 
 # lets convert samfile to bam first.
-samtools view -b -S SRR341449094.sam > SRR34149094_unsorted.bam
+#samtools view -b -S SRR34149094.sam > SRR34149094_unsorted.bam
 
 # lets convert unsorted bam to sorted bam
-samtools sort -o SRR34149094_sorted.bam SRR34149094_unsorted.bam
+# samtools sort -o SRR34149094_sorted.bam SRR34149094_unsorted.bam
 
 # lets index the sorted bamfiles
 
-samtools index  SRR34149094_sorted.bam
+# samtools index  SRR34149094_sorted.bam
 
 # Statistics of Aligned reads
-samtools flagstat SRR34149094_sorted.bam
+# samtools flagstat SRR34149094_sorted.bam
 
 # Step 6: Variant calling using GATK
+ref=/home/sne_desh/Ref/Homo_sapiens_grch38.fa
+python3 ~/Tools/gatk-4.6.2.0/gatk \
+   --java-options "-Xmx4g" HaplotypeCaller  \
+   -R $ref \
+   -I SRR34149094_sorted.bam \
+   -O SRR34149094.g.vcf.gz \
+   -ERC GVCF
 
